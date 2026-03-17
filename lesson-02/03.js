6903169dbddd6f905b74bda64ad219548876af27
VkorobchN <hellpraise17@gmail.com>
2026-03-17 21:56:25 +0300

    les02-03-first-commit

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,18 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+const score = 91 // тестовое значение, можно изменять
 let grade
 // your code
+
+if (score >= 90) {
+    grade = 'A' ;
+} else if (score >= 80) {
+    grade = 'B' ;
+} else if (score >= 70) {
+    grade = 'C' ;
+} else if (score >= 50) {
+    grade = 'D' ;
+} else {
+    grade = 'F' ;
+}
