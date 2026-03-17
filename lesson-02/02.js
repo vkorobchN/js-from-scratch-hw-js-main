ca8688cad5bd3c49890d9515ed6fe1137ced7e44
VkorobchN <hellpraise17@gmail.com>
2026-03-17 21:41:26 +0300

    les02-02-first-commit

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,9 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 6) // your code
+let dice2 = Math.floor(Math.random() * 6) // your code
+let isWinningDouble = dice1 === dice2// your code
 
 /*
 console.log('Первый бросок: ' + dice1)
