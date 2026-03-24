cd8b289762ac8157dc7aeef79ec8fcb0f3681f7c
VkorobchN <hellpraise17@gmail.com>
2026-03-24 19:48:14 +0300

    les03-first-commit

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,5 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage (a, b) {
+    return (a + b) /2
+}
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,5 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(a, b, c) {
+    return Math.max(a, b, c)
+}
