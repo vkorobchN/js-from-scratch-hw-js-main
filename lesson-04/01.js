6d9f233bcee3eefe0a686aa8b19977e455c1411b
VkorobchN <hellpraise17@gmail.com>
2026-04-06 20:41:49 +0300

    les04-01-first-commit

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,12 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(array. elment) {
+    for (let i = 0; i < array.length; i++) {
+    if (array[i] === element) {
+        return true
+        }
+    }
+    return false
+}
+
