354e78451c4ae7a62c7d5b30f63230168f65aec3
VkorobchN <hellpraise17@gmail.com>
2026-04-07 16:04:54 +0300

    les04-04-first-commit

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -13,4 +13,11 @@
 */
 
 function doubleEachCharacter(str) {
+        let result = '';
+
+    for (let i = 0; i < str.length; i++) {
+        result += str[i] + str[i];
+    }
+
+    return result;
 }
